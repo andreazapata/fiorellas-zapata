497b680621d7771f5aa304f3c0ca0af51cc5c185
andreazapata <andreazapatareza@gmail.com>
2022-07-02 19:30:27 -0500

    Tarea 4

--- fiorellas/src/components/ItemListContainer.js
+++ fiorellas/src/components/ItemListContainer.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 function ItemListContainer() {
+  const add = (cantidad) => {
+    alert("Agregaste " + cantidad + " producto(s) al carrito.");
+  };
   return (
     <>
       <p>Hola, soy el container :D </p>
+      <ItemCount onAdd={add} stock={20} initial={1} />
     </>
   );
 }
